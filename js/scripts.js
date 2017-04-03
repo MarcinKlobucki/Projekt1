01adce9a7589be24c06ac75b676f5b906828ee0d
MarcinKlobucki <mkfarfield@gmail.com>
2017-04-03 20:34:14 +0200

    zadanie 8.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+<<<<<<< Updated upstream
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+=======
+var a = prompt('Podaj wartosc dla a');
+	var b = prompt('Podaj wartosc dla b');
+		var value = (a * a) + (2 * a * b) - (b * b);
+
+console.log('Wynik działania wynosi: ' + value);
+
+if (value > 0) {
+
+    console.log('Wynik działania jest dodatni.');
+
+} else if (value < 0) {
+
+    console.log('Wynik działania jest ujemny.');
+} else {
+
+    console.log('Wynik działania jest równy 0.');
+
+}
+>>>>>>> Stashed changes
