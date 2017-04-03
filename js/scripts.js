d7ef796b1b9f4440d7d60d9b90999f8dcdb56a8d
MarcinKlobucki <mkfarfield@gmail.com>
2017-04-03 20:25:33 +0200

    zadanie 8.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = prompt('Wpisz wartość długości podstawy trójkąta');
+	var h = prompt('Wpisz wartość wysokości trójkąta');
+		var triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
