bfe3ce24490b1940622c634f12c686ae49d7717f
MarcinKlobucki <mkfarfield@gmail.com>
2017-04-10 22:34:33 +0200

    zadanie 8.8

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-	var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-		var allNames = femaleNames.concat(maleNames);
-			var newName ='Marian'
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.'
+	var animal = 'Zielone słonie'
+		var animalUpperCased = animal.toUpperCase();
+			var textCharsAfter = text.replace('Papugi', animalUpperCased); // nie wiem czy tutaj dobrze mogłem w sumie po prostu wpisać "ZIELONE SŁONIE" ale w zadaniu jest napisane, żeby zrobić po varze. jak coś to daj znać jak to umieścić żeby działało :)
+				var partOfTextCharsAfter = textCharsAfter.slice(0,length/2); // nie wiem czy to jest dobrze ale taka była podpowiedź w zadaniu, żeby zrobić "length/2"
 
-console.log(allNames);
-
-if (allNames.indexOf(newName) === -1) {
- 	allNames.push('Marian');
-}
+console.log(partOfTextCharsAfter);
