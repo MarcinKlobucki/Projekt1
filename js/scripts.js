f587dcbad5b1a1a5e9cd4bd77a8cf2adeed3a877
MarcinKlobucki <mkfarfield@gmail.com>
2017-04-11 22:37:42 +0200

    zadanie 8.8 poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na 
 	var animal = 'Zielone słonie'
 		var animalUpperCased = animal.toUpperCase();
 			var textCharsAfter = text.replace('Papugi', animalUpperCased); // nie wiem czy tutaj dobrze mogłem w sumie po prostu wpisać "ZIELONE SŁONIE" ale w zadaniu jest napisane, żeby zrobić po varze. jak coś to daj znać jak to umieścić żeby działało :)
-				var partOfTextCharsAfter = textCharsAfter.slice(0,length/2); // nie wiem czy to jest dobrze ale taka była podpowiedź w zadaniu, żeby zrobić "length/2"
+				var partOfTextCharsAfter = textCharsAfter.slice(0,textCharsAfter.length/2); // nie wiem czy to jest dobrze ale taka była podpowiedź w zadaniu, żeby zrobić "length/2"
 
 console.log(partOfTextCharsAfter);
