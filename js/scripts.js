4f063d9083a4a7070921b60169a9d85aa1677118
MarcinKlobucki <mkfarfield@gmail.com>
2017-04-14 00:13:52 +0200

    zadanie 8.9

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.'
-	var animal = 'Zielone słonie'
-		var animalUpperCased = animal.toUpperCase();
-			var textCharsAfter = text.replace('Papugi', animalUpperCased); // nie wiem czy tutaj dobrze mogłem w sumie po prostu wpisać "ZIELONE SŁONIE" ale w zadaniu jest napisane, żeby zrobić po varze. jak coś to daj znać jak to umieścić żeby działało :)
-				var partOfTextCharsAfter = textCharsAfter.slice(0,textCharsAfter.length/2); // nie wiem czy to jest dobrze ale taka była podpowiedź w zadaniu, żeby zrobić "length/2"
+function drawTree(h) {
 
-console.log(partOfTextCharsAfter);
+for ( var i = 1 ; i < h ; i++)
+{
+		var star = ' ';
+		for ( var j = 1 ; j < h*2-1 ; j++)
+		{
+			star += '*';
+		}
+		console.log(star)
+}
+}
